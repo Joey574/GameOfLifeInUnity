a5860f1c27fe6284d397037bf3a99d69a6c8331b
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2023-10-25 14:09:49 -0700

    working towards battle mode, scripts set up but not working, cells being turned white, no idea why

--- Game of Life Unity Project/Assets/Scripts/Battle Scripts/GameManagerBattle.cs
+++ Game of Life Unity Project/Assets/Scripts/Battle Scripts/GameManagerBattle.cs
@@ -11,16 +11,95 @@ public class GameManagerBattle : GameManagerTemplate
 
     protected override void setCellColor()
     {
-        throw new System.NotImplementedException();
     }
 
     protected override void inputHandler()
     {
-        throw new System.NotImplementedException();
+        if (Input.mouseScrollDelta.y != 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Input.mouseScrollDelta.y > 0)
+            {
+                radius += radiusInc;
+            }
+            else
+            {
+                radius -= radiusInc;
+            }
+
+            if (radius <= 0)
+            {
+                radius = 1;
+            }
+
+        }
+        else if (Input.mouseScrollDelta.y != 0)
+        {
+            scale.y = lastScale - (Input.mouseScrollDelta.y * zoomSensitivity);
+            scale.x = lastScale - (Input.mouseScrollDelta.y * zoomSensitivity);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            offset.x = lastOffset.x - (offsetInc * scale.x) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            offset.x = lastOffset.x + (offsetInc * scale.x) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            offset.y = lastOffset.y + (offsetInc * scale.y) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            offset.y = lastOffset.y - (offsetInc * scale.y) * Time.deltaTime;
+        }
+
+        paint = Input.GetMouseButton(0);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartCoroutine(callMenu());
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (red)
+            {
+                red = false;
+                blue = true;
+            }
+            else if (blue)
+            {
+                blue = false;
+            }
+            else
+            {
+                red = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Q)) { beginSim = !beginSim; }
     }
 
     protected override void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        throw new System.NotImplementedException();
+        if (!menuCalled)
+        {
+            if (paint)
+            {
+                float mouseX = Input.mousePosition.x * screenAdjustX * scale.x + (offset.x * textureWidth);
+                float mouseY = Input.mousePosition.y * screenAdjustY * scale.y + (offset.y * textureHeight);
+
+                toggleCellState.SetBool("paint", paint);
+                toggleCellState.SetBool("red", red);
+                toggleCellState.SetBool("blue", blue);
+                toggleCellState.SetFloat("radius", radius);
+                toggleCellState.SetFloat("mousePosX", mouseX);
+                toggleCellState.SetFloat("mousePosY", mouseY);
+
+                toggleCellState.Dispatch(0, threadDispatchX, threadDispatchY, 1);
+
+            }
+            Graphics.Blit(currentTexture, destination, scale, offset);
+        }
     }
 }
